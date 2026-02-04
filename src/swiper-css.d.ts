394277c1c408a4927830337fa0cfb318ce0aa776
Ziyad Tber <tberziad016@gmail.com>
2026-02-04 14:12:20 +0100

    fix the swiper js problem

---
+++ src/swiper-css.d.ts
@@ -0,0 +1,5 @@
+declare module 'swiper/css';
+declare module 'swiper/css/pagination';
+declare module 'swiper/css/autoplay';
+declare module 'swiper/css/navigation';
+declare module 'swiper/css/effect-fade';
